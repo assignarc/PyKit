307ca486cbde54b0e064171724163f8c109dac7d
vishalkhapre <vishal.khapre@live.com>
2025-11-17 22:05:41 -0600

    Renamed to fix case sensitive folder name

---
+++ src/VKPyKit1/__init__.py
@@ -0,0 +1,5 @@
+from ..VKPyKit1 import EDA
+from ..VKPyKit1 import DT
+
+__all__ = ["EDA", "DT"]
+# AIFunctions.addDT(2, 3)
--- src/vkpykit/__init__.py
+++
@@ -1,5 +0,0 @@
-from ..VKPyKit import EDA
-from ..VKPyKit import DT
-
-__all__ = ["EDA", "DT"]
-# AIFunctions.addDT(2, 3)
